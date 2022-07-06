f404b8aaedb4a3d027da7e244e48be007873a35d
Michel Metran <michelmetran@gmail.com>
2022-07-06 19:20:34 -0300

    ss

--- ufesp/read_data.py
+++ ufesp/read_data.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 import pandas as pd
-from paths import data_path
+from pathlib import Path
+
+# Paths
+project_path = Path(__file__).parents[1].resolve()
+data_path = project_path / 'ufesp' / 'data'
+data_path.mkdir(exist_ok=True)
+
+
+
+
+
+
+
 
 
 def get_table():
@@ -11,12 +24,11 @@ def get_table():
         parse_dates=['data_inicio', 'data_fim', 'ano_mes']
     )
     df.loc[:, 'ano_mes'] = pd.to_datetime(df['data_inicio']).dt.to_period('M')
-    print(df.head())
     return df
 
 
 def get_ufesp(dia):
-    #
+    # Get Dataframe
     df = get_table()
 
     # json
@@ -24,7 +36,17 @@ def get_ufesp(dia):
     return df.loc[mask].to_dict('records')[0]
 
 
+
+
+
+
+
+
 if __name__ == '__main__':
+    print(f'A pasta do projeto é: {project_path}')
+    print(f'A pasta dos dados é: {data_path}')
+
+    # 
     d = get_ufesp(dia='2021-11-15')
     print(d)
     print(d['valor'])
