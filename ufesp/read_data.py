50716b6245f3be60c4fb59cdc8872d9b5c524d7e
Michel Metran <michelmetran@gmail.com>
2022-07-19 14:37:32 -0300

    Ajuste

--- ufesp/read_data.py
+++ ufesp/read_data.py
@@ -1,39 +1,60 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+from datetime import datetime
 import pandas as pd
 from pathlib import Path
 
-# Paths
-project_path = Path(__file__).parents[1].resolve()
-data_path = project_path / 'ufesp' / 'data'
-data_path.mkdir(exist_ok=True)
-
 
 def get_table():
+    # Paths
+    project_path = Path(__file__).parents[1].resolve()
+    data_path = project_path / 'ufesp' / 'data'
+    data_path.mkdir(exist_ok=True)
+
+    # ddd
     df = pd.read_csv(
         data_path / 'ufesp.csv',
-        parse_dates=['data_inicio', 'data_fim', 'ano_mes']
+        parse_dates=['data_inicio', 'data_fim', 'ano_mes'],
+        decimal=','
     )
     df.loc[:, 'ano_mes'] = pd.to_datetime(df['data_inicio']).dt.to_period('M')
     return df
 
 
-def get_ufesp(dia):
+def get_ufesp_from_date(date):
     # Get Dataframe
     df = get_table()
 
-    # json
-    mask = (df['data_inicio'] <= dia) & (df['data_fim'] >= dia)
+    # Json
+    mask = (df['data_inicio'] <= date) & (df['data_fim'] >= date)
+    return df.loc[mask].to_dict('records')[0]
+
+
+def get_ufesp_from_year(year):
+    # Get Dataframe
+    df = get_table()
+
+    # Create Year Columns
+    df['data_inicio_year'] = pd.DatetimeIndex(df['data_inicio']).year
+    df['data_fim_year'] = pd.DatetimeIndex(df['data_fim']).year
+
+    # Json
+    mask = (df['data_inicio_year'] <= year) & (df['data_fim_year'] >= year)
     return df.loc[mask].to_dict('records')[0]
 
 
 if __name__ == '__main__':
-    print(f'A pasta do projeto é: {project_path}')
-    print(f'A pasta dos dados é: {data_path}')
+    # Com dia (string)
+    dados = get_ufesp_from_date(date='2020-11-15')
+    print(dados['valor'])
 
-    # 
-    d = get_ufesp(dia='2021-11-15')
-    print(d)
-    print(d['valor'])
+    # Com dia (datetime)
+    a = '2021-11-15'
+    b = datetime.strptime(a, '%Y-%m-%d')
+    dados = get_ufesp_from_date(date=b)
+    print(dados['valor'])
+
+    # Com Ano
+    dados = get_ufesp_from_year(2022)
+    print(dados['valor'])
